35ddb4b37ff3192c45228f7cca12cf72c5be0bb0
MaxEtmanov <166302161+MaxEtmanov@users.noreply.github.com>
2024-08-11 19:36:20 +0400

    Add files via upload

---
+++ manager.py
@@ -0,0 +1,28 @@
+import whisper
+from pprint import pprint
+import time
+import matplotlib.pyplot as plt
+
+
+def score_time(model):
+    model = whisper.load_model(model)
+    start = time.time()
+    result = model.transcribe("manager.mp3")
+    end = time.time()
+    return end - start
+
+models = ["tiny", "base", "small", "medium", "large"]
+
+times = []
+for model in models:
+    transcription_time = score_time(model)
+    times.append(transcription_time)
+    print(f"{model} model: {transcription_time:.2f} seconds")
+    
+    
+plt.figure(figsize=(10, 6))
+plt.bar(models, times)
+plt.title("срванение скорости")
+plt.xlabel("модель")
+plt.ylabel("время")
+plt.show()
