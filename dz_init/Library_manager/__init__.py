777241a95c80644032d6ce7e84a2c6de500967ca
MaxEtmanov <166302161+MaxEtmanov@users.noreply.github.com>
2024-08-22 15:32:21 +0400

    Add files via upload

--- dz_init/Library_manager/__init__.py
+++ dz_init/Library_manager/__init__.py
@@ -1,8 +1,15 @@
 
-# from catalog import *
-
-# from repotr import *
-# # Определение __all__ для контроля того, что импортируется при использовании from ... import *
-# __all__ = ['Library ', 'generate_report']
 
 
+
+# Определение __all__ для контроля того, что импортируется при использовании from ... import *
+__all__ = ['validate_book', 'format_book_data']
+
+# Опционально: добавьте сообщение для отслеживания импорта
+print("Модуль Library_manager.utils импортирован")
+
+# Импорт из data_validation.py
+from utils.data_validation import validate_book
+
+
+from utils.formatting import format_book_data
